45511c5afa6a01e18c6bfe9b0d187d842d410360
SirScotterington <scott7gilbert@gmail.com>
2025-06-30 20:35:45 -0500

    Fix API routing

---
+++ api/fetch-users/index.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405); // Method Not Allowed
+  echo json_encode(['success' => false, 'error' => 'Only POST is allowed']);
+  exit;
+}
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Autoload files (we’ll make this work later)
+spl_autoload_register(function ($class) {
+  $paths = ['controllers', 'models', 'core'];
+  foreach ($paths as $path) {
+    $file = __DIR__ . "../backend/$path/$class.php";
+    if (file_exists($file)) {
+      require_once $file;
+      return;
+    }
+  }
+});
+
+$controller = new UserController();
+$controller->fetchUsers();
---
+++ api/terminate-session/index.php
@@ -0,0 +1,35 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405); // Method Not Allowed
+  echo json_encode(['success' => false, 'error' => 'Only POST is allowed']);
+  exit;
+}
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Autoload files (we’ll make this work later)
+spl_autoload_register(function ($class) {
+  $paths = ['controllers', 'models', 'core'];
+  foreach ($paths as $path) {
+    $file = __DIR__ . "../backend/$path/$class.php";
+    if (file_exists($file)) {
+      require_once $file;
+      return;
+    }
+  }
+});
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+foreach ($input as $key => $value) {
+  $value = trim($value);
+  $value = stripslashes($value);
+  $value = htmlspecialchars($value);
+
+  $input[$key] = $value;
+}
+
+$controller = new SessionController();
+$controller->terminateSession($input);
